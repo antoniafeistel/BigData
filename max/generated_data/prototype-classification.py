00a3ce52162d953bdb8b740e04363528664a0906
Hoffmann <max.wnstr@gmail.com>
2023-11-14 21:54:45 +0100

    jetzt gehts

--- max/generated_data/prototype-classification.py
+++ max/generated_data/prototype-classification.py
@@ -13,7 +13,7 @@ spark = SparkSession.builder.master("spark://N279WMVDJ2:7077").getOrCreate()
 # Initialisiere die Spark-Sitzung
 #spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
 
-csv_file_path = './generated_data/adults_50up_female_urban_6-7.csv'
+csv_file_path = './max/generated_data/adults_50up_female_urban_6-7.csv'
 
 # Lade das CSV-Datei in ein DataFrame
 dataFrame = spark.read.csv(csv_file_path, header=True, inferSchema=True, sep='|')
@@ -23,6 +23,8 @@ feature_cols = [ 'cc_num', "lat", "long" ]
 #feature_cols.remove("is_fraud")  # Entferne die Label-Spalte
 vector_assembler = VectorAssembler(inputCols=feature_cols, outputCol="features")
 dataFrame = vector_assembler.transform(dataFrame)
+
+dataFrame.show()
 
 # Aufteilung der Daten in Trainings- und Testsets
 (trainingData, testData) = dataFrame.randomSplit([0.7, 0.3])
