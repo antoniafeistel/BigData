26661f3177259d386e815182e95229329b25449c
Daniel Schneider <daniel-wernau@t-online.de>
2023-11-19 21:19:01 +0100

    Incorporate files wrt Kafka streaming into own Python package

---
+++ kafka_streaming/config.py
@@ -0,0 +1,5 @@
+KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
+KAFKA_TOPIC = 'avro_test'
+CSV_FILE_PATH = '/files/test.csv'
+AVRO_FILE_PATH = '/files/test.avro'
+SPARK_MASTER = "spark://LFK66VG60V:7077"
