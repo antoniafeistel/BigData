37c60d9cb53e7de2446d715504ebf945b0d5dc25
Daniel Schneider <daniel-wernau@t-online.de>
2023-12-04 21:02:00 +0100

    Push

--- model_training/prototype-classification.py
+++ model_training/prototype-classification.py
@@ -15,16 +15,17 @@ spark = SparkSession.builder.master(config.SPARK_MASTER).getOrCreate()
 # Initialisiere die Spark-Sitzung
 #spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
 
-csv_file_path = './max/generated_data/adults_50up_female_urban_6-7.csv'
+csv_file_path = './max/generated_data/data/adults_50up_female_rural_4-5 copy.csv'
 
 # Lade das CSV-Datei in ein DataFrame
 dataFrame = spark.read.csv(csv_file_path, header=True, inferSchema=True, sep='|')
-featuresCol = list(dataFrame.columns[:-1])
+#featuresCol = list(dataFrame.columns[:-1])
+featuresCol = ["cc_num", "lat", "long"]
 
 #feature_cols.remove("is_fraud")  # Entferne die Label-Spalte
 
 vector_assembler = VectorAssembler(inputCols=featuresCol, outputCol="features")
-df = SQLContext(spark).createDataFrame(dataFrame)
+# dataFrame = SQLContext(spark).createDataFrame(dataFrame)
 dataFrame = vector_assembler.transform(dataFrame)
 dataFrame = dataFrame.select("features", "is_fraud")
 
