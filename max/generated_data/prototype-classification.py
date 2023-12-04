76719bfdcf0f569b0c95173ffcabe43c0172dd7d
Feistel <antonia@feistel.info>
2023-12-04 20:23:19 +0100

    Prototype Training/Classifaction Implement Model Training

--- max/generated_data/prototype-classification.py
+++ max/generated_data/prototype-classification.py
@@ -2,6 +2,7 @@ import findspark
 findspark.init()
 
 import pyspark
+from config import config
 from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
@@ -9,7 +10,7 @@ from pyspark.ml.feature import VectorAssembler, StringIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 
-spark = SparkSession.builder.master("spark://N279WMVDJ2:7077").getOrCreate()
+spark = SparkSession.builder.master(config.SPARK_MASTER).getOrCreate()
 # Initialisiere die Spark-Sitzung
 #spark = SparkSession.builder.appName("FraudDetection").getOrCreate()
 
@@ -17,23 +18,17 @@ csv_file_path = './max/generated_data/adults_50up_female_urban_6-7.csv'
 
 # Lade das CSV-Datei in ein DataFrame
 dataFrame = spark.read.csv(csv_file_path, header=True, inferSchema=True, sep='|')
-
-feature_cols = [ 'cc_num', "lat", "long" ]
+feature_columns = list(dataFrame.columns[:-1])
 
 #feature_cols.remove("is_fraud")  # Entferne die Label-Spalte
+
 vector_assembler = VectorAssembler(inputCols=feature_cols, outputCol="features")
+df = SQLContext(spark).createDataFrame(dataFrame)
 dataFrame = vector_assembler.transform(dataFrame)
+dataFrame = dataFrame.select("features", "is_fraud")
 
-dataFrame.show()
-
-# Aufteilung der Daten in Trainings- und Testsets
-(trainingData, testData) = dataFrame.randomSplit([0.7, 0.3])
-
-rf = RandomForestClassifier(labelCol="is_fraud", featuresCol="features", numTrees=10)
-model = rf.fit(trainingData)
-
-# Mache Vorhersagen
-predictions = model.transform(testData)
+rf = RandomForestClassifier(labelCol="is_fraud", featuresCol="features", predictionCol="prediction", numTrees=10)
+model = rf.fit(dataFrame)
 
 # Evaluierung des Modells
 evaluator = MulticlassClassificationEvaluator(
@@ -45,7 +40,5 @@ print("Test Accuracy = %g" % accuracy)
 rfModel = model.trees
 print(rfModel)  # summary only
 
-model_path = "./max/generated_data/model"
-model.write().overwrite().save(model_path)
+model.write().overwrite().save(config.MODEL_PATH)
 
-spark.stop()
