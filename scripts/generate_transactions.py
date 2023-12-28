70f5518037ec3853244e86e6458d65183cbc6d37
Daniel Schneider <daniel-wernau@t-online.de>
2023-12-28 19:20:00 +0100

    Write script for generating transaction data continously

--- scripts/generate_transactions.py
+++ scripts/generate_transactions.py
@@ -1,6 +1,28 @@
 import subprocess
 import argparse
 import sys
+import os
+import shutil
+import csv
+
+
+def is_csv_header_only(file_path):
+    with open(file_path, 'r', newline='') as file:
+        csv_reader = csv.reader(file)
+        # jump over header
+        next(csv_reader)
+        return all(row == [] for row in csv_reader)
+
+
+def get_non_empty_transactions(source_folder, destination_folder):
+    for file_name in os.listdir(source_folder):
+        source_path = os.path.join(source_folder, file_name)
+        destination_path = os.path.join(destination_folder, file_name)
+
+        if 'adults' in file_name.lower() and not is_csv_header_only(source_path):
+            shutil.move(source_path, destination_path)
+        else:
+            os.remove(source_path)
 
 
 def generate_transactions(n, o, s, e):
@@ -9,11 +31,15 @@ def generate_transactions(n, o, s, e):
 
 
 if __name__ == "__main__":
+    test_temp_folder = "../resources/data/test_temp"
+
     parser = argparse.ArgumentParser(description='Generate transactions with specified parameters.')
     parser.add_argument('-n', type=str, help='Number of customers to generate', default='10')
-    parser.add_argument('-o', type=str, help='Output folder path', default="../resources/data/test")
+    parser.add_argument('-o', type=str, help='Output folder path', default=test_temp_folder)
     parser.add_argument('-s', type=str, help='Transactions start date in the format "%m-%d-%Y"', default='01-01-2015')
     parser.add_argument('-e', type=str, help='Transactions end date in the format "%m-%d-%Y"', default='01-01-2020')
     args = parser.parse_args()
 
-    generate_transactions(args.n, args.o, args.s, args.e)
+    while True:
+        generate_transactions(args.n, args.o, args.s, args.e)
+        get_non_empty_transactions(test_temp_folder, "../resources/data/test")
